6baf9f2be38a81cc4ff32cab5324f6f15f567617
Sara Gasperini <sgasperi@e4r2p1.42roma.it>
2024-03-27 17:19:05 +0100

    WIP get_next_line

--- exam/get_next_line/get_next_line.c
+++ exam/get_next_line/get_next_line.c
@@ -1,4 +1,17 @@
-/*strjoin, strdup, strlcpy, strchr*/
+/*strjoin, strdup*/
+#include "get_next_line.h"
+
+char	ft_strchr(char *str, char c)
+{
+	int	i = 0;
+	while (str[i])
+	{
+		if (str[i] == c)
+			return (str[i]);
+		i++;
+	}
+	return (NULL);
+}
 
 int	ft_strlen(char *str)
 {
@@ -8,7 +21,19 @@ int	ft_strlen(char *str)
 	return (i);
 }
 
-char	*ft_strlcpy(char *dst, char *src, int len)
+char	*ft_strlcpy(char *dst, char *src, int dstlen)
 {
-	
+	int	i = 0;
+	int	srclen = ft_strlen(src);
+
+	if (dstlen > 0)
+	{
+		while(i < srclen && i < dstlen)
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
+	}
+	return (dst);
 }
