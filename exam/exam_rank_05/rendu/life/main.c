30a74fba457efb3babf60fbafe0632f20d26fe37
Sara Gasperini <sgasperi@e4r11p2.42roma.it>
2026-03-20 16:27:24 +0100

    WIP life

--- exam/exam_rank_05/rendu/life/main.c
+++ exam/exam_rank_05/rendu/life/main.c
@@ -5,20 +5,38 @@
 //Allowed functions: atoi, read, putchar, malloc, realloc, calloc, free
 
 int main (int ac, char **av) {
-    int height = 0;
-    int width = 0;
-    int iterations = 0;
-    if (ac == 4) {
-        height = atoi(av[2]);
-        width = atoi(av[1]);
-        iterations = atoi(av[3]);
+    if (ac != 4) return (1);
+    height = atoi(av[2]);
+    width = atoi(av[1]);
+    iterations = atoi(av[3]);
+    char alive = '0';
+    char dead = ' ';
+    char **map = (char **)malloc(sizeof(char *) * height);
+    if (!map || !matrix) return (1);
+    (for int i = 0; i < height; i++) {
+        map[i] = (char *)malloc(sizeof(char) * width);
+        matrix[i] = (int *)malloc(sizeof(int) * width);
+        if (!map[i] || !matrix[i]) return (1);
     }
-    else if (ac == 2) {
-
+    
+    char comm;
+    int pen = 0;
+    int flag = 0;
+    int py = 0;
+    int px = 0;
+    while (read(STDIN_FILENO, &comm) == 1) {
+        switch(comm) {
+            case('x'): { pen = !pen; break ;}
+            case('w'): { if (pen && py > 0) py--; break ; }
+            case('s'): { if (pen && py < height) py++; break ;}
+            case('a'): { if (pen && px > 0) px--; break ;}
+            case('d'): { if (pen && px < width) px++; break ;}
+            default: { flag = 1; }
+        }
+        if (pen == 1 && flag == 0) {
+            if (py > 0 && py <= height && px > 0 && px <= width)
+                map[py][px] = alive
+        }
     }
-    char *comms = NULL;
-    ssize_t nread = read(stdin, comms);
-    if (nread == -1) return (1);
-    char **tmp;
     
 }
