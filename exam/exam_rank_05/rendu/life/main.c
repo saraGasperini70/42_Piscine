2c1b0ba6450503713a7cabeeee4690b97436cd63
SGasperi <121606898+saraGasperini70@users.noreply.github.com>
2026-03-28 10:24:58 +0100

    Update main.c

--- exam/exam_rank_05/rendu/life/main.c
+++ exam/exam_rank_05/rendu/life/main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
 
 int **malloc_2d_array(int height, int width, int need_init) {
     int **array = (int **)malloc(sizeof(int *) * height);
